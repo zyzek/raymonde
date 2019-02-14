c530854b5ce0d3d277323254d028afb21a4c9395
Anton Jurisevic <ajur4521@uni.sydney.edu.au>
2019-02-14 12:46:42 +1100

    Sine plane stuff.

---
+++ SinePlane.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Anton Jurisevic on 2019-02-13.
+//
+
+#include "SinePlane.h"
---
+++ SinePlane.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Anton Jurisevic on 2019-02-13.
+//
+
+#ifndef RAYMONDE_SINEPLANE_H
+#define RAYMONDE_SINEPLANE_H
+
+
+class SinePlane {
+
+};
+
+
+#endif //RAYMONDE_SINEPLANE_H
